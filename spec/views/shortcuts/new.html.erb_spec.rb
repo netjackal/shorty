d0b6c35bb0355e4ce941b20bd212d3c469e10e8d
Kelvin Teh <register@netjackal.net>
2011-08-29 18:31:12 +0800

    [wip] created view spec and view template
    skeletal template just to pass the rspec
    next: add form elements to the view template
    
    cucumber is failing cos it cannot find the url field to fill in

---
+++ spec/views/shortcuts/new.html.erb_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+# require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe "shortcuts/new.html.erb" do
+  it "should render the new shortcut form correctly" do
+    render
+    rendered.should have_content("Hello")
+    # rendered.should have_selector("form",
+    #                                 :method => "post",
+    #                                 :action => "create_shortcut_path"
+    #                              )
+  end
+end
