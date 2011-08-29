11025fe1535ceefce53c4c7dded0bfc216b2b517
Kelvin Teh <register@netjackal.net>
2011-08-30 01:30:14 +0800

    [wip] modified for more realistic scenario in view
    next: add model object in controller to pass to view
    
    complains about missing model

--- spec/views/shortcuts/new.html.erb_spec.rb
+++ spec/views/shortcuts/new.html.erb_spec.rb
@@ -4,7 +4,8 @@ require 'spec_helper'
 describe "shortcuts/new.html.erb" do
   it "should render the new shortcut form correctly" do
     render
-    rendered.should have_content("Hello")
+
+    rendered.should have_field?("url")
     # rendered.should have_selector("form",
     #                                 :method => "post",
     #                                 :action => "create_shortcut_path"
