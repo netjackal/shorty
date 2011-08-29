c328ccf48d856743af92a1df9db2b7279eb2fa36
Kelvin Teh <register@netjackal.net>
2011-08-29 17:38:49 +0800

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shorty::Application.config.secret_token = '29576b1faad8ba7bd237a67f76e7e8039441574c1e39c8e05cd6910a4cb90b544be837a98a54e1ef8b3d272ac8a0480d484ec2e355bd49b75ce1c2bf04eb0280'
