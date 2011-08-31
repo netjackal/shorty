4332ab9393e5b3fd3436e756ab2814b8d2bb0d11
Kelvin Teh <register@netjackal.net>
2011-08-31 12:01:36 +0800

    [wip] first cut at creating shortcuts migration
    next:
    
    cucumber failing not finding shortcuts path

---
+++ db/migrate/20110831031226_create_shortcuts.rb
@@ -0,0 +1,14 @@
+class CreateShortcuts < ActiveRecord::Migration
+  def self.up
+    create_table(:shortcuts) do |t|
+      t.string    :url
+      t.string    :hash
+      t.integer    :visit_count, :default => 0
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+  end
+end
