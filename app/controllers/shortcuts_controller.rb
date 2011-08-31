eee7c34debd1ae55e6b58434e04e784354be0f01
Kelvin Teh <register@netjackal.net>
2011-08-31 16:56:54 +0800

    [wip] made shortcut controller pass controller spec for create
    next: add index action to controller and code
    
    cucumber complains that it cannot find the index action for shortcut

--- app/controllers/shortcuts_controller.rb
+++ app/controllers/shortcuts_controller.rb
@@ -2,4 +2,12 @@ class ShortcutsController < ApplicationController
   def new
     @shortcut = Shortcut.new
   end
+
+  def create
+    @shortcut = Shortcut.new(params[:shortcut])
+    @shortcut.hash = '/qwe123'
+    if @shortcut.save
+      redirect_to shortcuts_path
+    end
+  end
 end
--- spec/controllers/shortcuts_controller_spec.rb
+++ spec/controllers/shortcuts_controller_spec.rb
@@ -3,14 +3,26 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe ShortcutsController do
   # render_views
   describe "#new" do
-    before do
-      get :new
-    end
     it "should return a successful response" do
+      get :new
       response.should be_success
+    end
+    it "should create a new blank shortcut object" do
+      get :new
+      assigns(:shortcut).should_not be nil
+    end
+  end
+  describe "#create" do
+    before do
+      post :create, "shortcut" => { "url"=>"http://goo.gl" }
     end
     it "should create a new shortcut object" do
       assigns(:shortcut).should_not be_nil
+      assigns(:shortcut).url.should == "http://goo.gl"
+      assigns(:shortcut).hash.should == "/qwe123"
+    end
+    it "should redirect to list shortcut page" do
+      response.should redirect_to shortcuts_path
     end
   end
 end
