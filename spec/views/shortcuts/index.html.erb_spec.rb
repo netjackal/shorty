bb1afdae4c06b0b6ab0b48de43dbaa9bdd4a7f95
Kelvin Teh <register@netjackal.net>
2011-08-31 18:35:36 +0800

    [wip] fixed problem with empty dataset passed to template

--- spec/views/shortcuts/index.html.erb_spec.rb
+++ spec/views/shortcuts/index.html.erb_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 describe "shortcuts/index.html.erb" do
   it "should render the list shortcuts page correctly" do
-    Shortcut.create!(:url=>"http://one", :hash=>"/qqq", :visit_count=>2)
+    sc = Shortcut.create!(:url=>"http://one", :hash=>"/qqq", :visit_count=>2)
+    assign(:shortcuts,[sc])
     render
-    rendered.should contain("URL")
+    rendered.should have_content("URL")
     rendered.should have_content("Hash")
     rendered.should have_content("Count")
   end
